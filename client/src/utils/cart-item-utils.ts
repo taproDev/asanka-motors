4a18558184eefc27a9dc2159f3d0cea64d8a0378
sahan madusha <sahanmadusha001@gmail.com>
2024-02-08 15:51:21 +0530

    bug fixed cart item add remove

--- client/src/utils/cart-item-utils.ts
+++ client/src/utils/cart-item-utils.ts
@@ -12,12 +12,21 @@ let cartItems: CartItem[] = [];
 export const useCart = () => {
   const [isCartAdded, setIsCartAdded] = useState(false);
 
-  //Function to add item to cart
-  const addToCart = (productId: number, price: number, qty: number) => {
-    const newItem: CartItem = { productId, price, qty };
+// Function to add item to cart
+const addToCart = (productId: number, price: number, qty: number) => {
+  const newItem: CartItem = { productId, price, qty };
+  
+  const cartItemsString = sessionStorage.getItem("cartItems");
+  const cartItems_sess = cartItemsString ? JSON.parse(cartItemsString) : [];
+
+  if (cartItems_sess.length === 0) {
     cartItems.push(newItem);
-    updateSessionStorage();
-  };
+  } else {
+    cartItems_sess.push(newItem);
+    updateSessionStorage(cartItems_sess);
+  }
+};
+
 
   //cart remove
   const removeFromCart = (productid: number) => {
@@ -30,18 +39,19 @@ export const useCart = () => {
         ...cartItems_rem.slice(index + 1),
       ];
       cartItems = updatedCartItems;
-      updateSessionStorage();
+      updateSessionStorage(updatedCartItems);
     } else {
-      alert("Something went wrong, Please try again!"+index);
+      alert("Something went wrong, Please try again!");
     }
   };
 
   //tempoy cart item store in sesssion
-  const updateSessionStorage = () => {
-    sessionStorage.setItem("cartItems", JSON.stringify(cartItems));
+  const updateSessionStorage = (updatedCartItems:any) => {
+    sessionStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
   };
+  
 
-  return { cartItems, addToCart, removeFromCart,isCartAdded, setIsCartAdded};
+  return { cartItems, addToCart, removeFromCart, isCartAdded, setIsCartAdded };
 };
 
 //read session data
